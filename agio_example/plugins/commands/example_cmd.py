8f38f9547df2955907bbdfbad06f7e97e8f4fbfa
paulwinex <paulwinex@gmail.com>
2025-10-04 21:17:29 +0300

    update example

--- agio_example/plugins/commands/example_cmd.py
+++ agio_example/plugins/commands/example_cmd.py
@@ -9,6 +9,7 @@ class SimpleCommand(ACommandPlugin):
     name = 'example_cmd'
     command_name = 'example'
     arguments = [
+        # use click "argument" and "option" functions to set up command arguments
         click.option("-a", "--arg-a", is_flag=True, help='Bool flag'),
         click.option("-b", "--arg-b", help='String argument'),
         click.option('-c', '--arg-c', help='Path Argument',
@@ -16,13 +17,16 @@ class SimpleCommand(ACommandPlugin):
                      default=Path.cwd().absolute().as_posix()
                      ),
     ]
+    allow_extra_args = True
+    # catch all arguments and get it in __extra_args__ value
 
-    def execute(self, arg_a: bool, arg_b: str, arg_c: str):
+    def execute(self, arg_a: bool, arg_b: str, arg_c: str, __extra_args__: list = None):
         print('='*50)
         print('Simple Command')
         print(f'arg_a: {arg_a}')
         print(f'arg_b: {repr(arg_b)}')
         print(f'arg_c: {arg_c}')
+        print('Extra args:', __extra_args__)
         print('='*50)
         emit('example.command.event1', 1)
         emit('example.command.event2', 2)
