3e6c2ce9394cbb02a226d9ec0d95fee21f30eea1
paulwinex <paulwinex@gmail.com>
2025-10-14 11:27:36 +0300

    refactor callbacks

--- agio_example/callbacks/example1.py
+++ agio_example/callbacks/example1.py
@@ -1,15 +1,16 @@
-from agio.core.events import callback
+from agio.core.events import callback, AEvent
+
 
 @callback('agio.init.done')
-def on_core_init(event, payload=None):
+def on_core_init(event: AEvent):
     print('CORE INIT DONE!!!!!!!!', event)
 
 
 @callback('agio.init.package_loaded')
-def on_package_load(event, pkg):
-    print('PACKAGE LOADED', pkg)
+def on_package_load(event: AEvent):
+    print('PACKAGE LOADED', event.payload['package'])
 
 
 @callback('example.command*')
-def on_example_command(event, payload):
-    print('Example Event', payload)
+def on_example_command(event: AEvent):
+    print('Example Event', event.payload)
