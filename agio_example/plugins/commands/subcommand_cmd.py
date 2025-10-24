c7f725b24aed9bd11a8095f05a644c98a9919f61
paulwinex <paulwinex@gmail.com>
2025-10-24 18:19:13 +0300

    added subcommands example

---
+++ agio_example/plugins/commands/subcommand_cmd.py
@@ -0,0 +1,30 @@
+from agio.core.plugins.base_command import ACommandPlugin, ASubCommand
+import click
+
+
+class SubCommand1(ASubCommand):
+    command_name = "cmd1"
+
+    def execute(self,  **kwargs):
+        click.secho(f'Command 1', fg='yellow')
+
+
+class SubCommand2(ASubCommand):
+    command_name = "cmd2"
+
+    def execute(self,  **kwargs):
+        click.secho(f'Command 2', fg='yellow')
+
+
+class ExampleSubCommand(ACommandPlugin):
+    name = 'example_sub_cmd'
+    command_name = "subcmd"
+    subcommands = [
+        SubCommand1,
+        SubCommand2,
+    ]
+    help = 'Example sub command'
+    allow_empty_root_command = True
+
+    def execute(self, **kwargs):
+        click.secho(f'Root Command', fg='yellow')
